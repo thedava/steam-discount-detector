0bff71f76962220efa829f2fa693980b6612cc22
David S <thedava@users.noreply.github.com>
2016-12-12 14:52:12 +0100

    Create detect.php

---
+++ detect.php
@@ -0,0 +1,9 @@
+<?php
+
+$url = 'http://store.steampowered.com/app/376850/';
+$selector = '.game_area_purchase_game_dropdown_menu_items_container .game_area_purchase_game_dropdown_menu_item_discount';
+$cookies = [
+  'steamCountry'    => 'DE%7C16b3e386c80e042e41df9fb917a17dbf',
+  'birthtime'       => 318207601,
+  'lastagecheckage' => '01-Februar-1980',
+];
