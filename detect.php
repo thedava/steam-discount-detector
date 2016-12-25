5982d73402420440d91dee1a467eb3b4c50640c9
David S <thedava@users.noreply.github.com>
2016-12-25 17:50:48 +0100

    Update detect.php

--- detect.php
+++ detect.php
@@ -1,7 +1,9 @@
 <?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 $url = 'http://store.steampowered.com/app/376850/';
-$selector = '.game_area_purchase_game_dropdown_menu_items_container .game_area_purchase_game_dropdown_menu_item_discount';
+$selector = '.game_area_purchase_game_dropdown_menu_items_container .game_area_purchase_game_dropdown_menu_item_savings';
 $cookies = [
   'steamCountry'    => 'DE%7C16b3e386c80e042e41df9fb917a17dbf',
   'birthtime'       => 318207601,
